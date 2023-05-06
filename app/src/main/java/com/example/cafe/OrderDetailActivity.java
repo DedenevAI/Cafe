0e8057511fe4c6ec0925ee0c5de4b0b2f349ba1a
alexMac <dedenevai3263@yandex.ru>
2023-05-06 22:25:14 +0300

    OrderDetailActivity layout completed

--- app/src/main/java/com/example/cafe/OrderDetailActivity.java
+++ app/src/main/java/com/example/cafe/OrderDetailActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -12,12 +13,19 @@ public class OrderDetailActivity extends AppCompatActivity {
     public static final String EXTRA_USER_USERNAME = "userName";
     public static final String EXTRA_USER_DRINK = "drink";
     public static final String EXTRA_USER_ADDITIVES = "additives";
-    public static final String EXTRA_USER_DRINK_TYPE = "userName";
+    public static final String EXTRA_USER_DRINK_TYPE = "drinkType";
+    private TextView textViewName;
+    private TextView textViewDrink;
+    private TextView textViewDrinkType;
+    private TextView textViewAdditives;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+        initViews();
+
+        setUpValues();
     }
 
     public static Intent newIntent(Context context,
@@ -28,8 +36,22 @@ public class OrderDetailActivity extends AppCompatActivity {
         Intent intent = new Intent(context, OrderDetailActivity.class);
         intent.putExtra(EXTRA_USER_USERNAME, userName);
         intent.putExtra(EXTRA_USER_DRINK, drink);
-        intent.putExtra(EXTRA_USER_DRINK, additives);
+        intent.putExtra(EXTRA_USER_ADDITIVES, additives);
         intent.putExtra(EXTRA_USER_DRINK_TYPE, drinkType);
         return intent;
+    }
+
+    private void initViews() {
+        textViewName = findViewById(R.id.textViewName);
+        textViewDrink = findViewById(R.id.textViewDrink);
+        textViewDrinkType = findViewById(R.id.textViewDrinkType);
+        textViewAdditives = findViewById(R.id.textViewAdditives);
+    }
+
+    private void setUpValues() {
+        textViewName.setText(getIntent().getStringExtra(EXTRA_USER_USERNAME));
+        textViewDrink.setText(getIntent().getStringExtra(EXTRA_USER_DRINK));
+        textViewDrinkType.setText(getIntent().getStringExtra(EXTRA_USER_DRINK_TYPE));
+        textViewAdditives.setText(getIntent().getStringExtra(EXTRA_USER_ADDITIVES));
     }
 }
