5988c37f9bfe1059ed817adb4716919f646a3178
alexMac <dedenevai3263@yandex.ru>
2023-05-06 22:12:56 +0300

    MakeOrderActivity completed

--- app/src/main/java/com/example/cafe/MakeOrderActivity.java
+++ app/src/main/java/com/example/cafe/MakeOrderActivity.java
@@ -5,17 +5,131 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MakeOrderActivity extends AppCompatActivity {
+    private static final String EXTRA_USER_NAME = "userName";
+    private String drink;
+    private String userName;
+    private TextView textViewGreetings;
+    private TextView textViewAdditives;
+
+    private RadioGroup radioGroupDrinks;
+    private RadioButton radioButtonTea;
+    private RadioButton radioButtonCoffee;
+
+    private CheckBox checkboxSugar;
+    private CheckBox checkboxMilk;
+    private CheckBox checkboxLemon;
+
+    private Spinner spinnerTea;
+    private Spinner spinnerCoffee;
+
+    private Button buttonMakeOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order);
+        initViews();
+        setUpUserName();
+        radioGroupDrinks.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int id) {
+                if (id == radioButtonTea.getId()) {
+                    onUsersChooseTea();
+                } else if (id == radioButtonCoffee.getId()) {
+                    onUsersChooseCoffee();
+                }
+            }
+        });
+        radioButtonTea.setChecked(true);
+        buttonMakeOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onUserMakeOrder();
+            }
+        });
     }
-    public static Intent newIntent(Context context, String userName){
+
+    public static Intent newIntent(Context context, String userName) {
         Intent intent = new Intent(context, MakeOrderActivity.class);
-        intent.putExtra("userName", userName);
+        intent.putExtra(EXTRA_USER_NAME, userName);
         return intent;
+    }
+
+    private void initViews() {
+        textViewGreetings = findViewById(R.id.greetings);
+        textViewAdditives = findViewById(R.id.textViewAdditives);
+
+        radioGroupDrinks = findViewById(R.id.radioGroupDrinks);
+        radioButtonTea = findViewById(R.id.radioButtonTea);
+        radioButtonCoffee = findViewById(R.id.radioButtonCoffee);
+
+        checkboxSugar = findViewById(R.id.checkboxSugar);
+        checkboxMilk = findViewById(R.id.checkboxMilk);
+        checkboxLemon = findViewById(R.id.checkboxLemon);
+
+        spinnerCoffee = findViewById(R.id.spinnerCoffee);
+        spinnerTea = findViewById(R.id.spinnerTea);
+
+        buttonMakeOrder = findViewById(R.id.buttonMakeOrder);
+    }
+
+    private void setUpUserName() {
+        userName = getIntent().getStringExtra(EXTRA_USER_NAME);
+        String greetings = getString(R.string.greetings, userName);
+        textViewGreetings.setText(greetings);
+    }
+
+    private void onUsersChooseTea() {
+        drink = getString(R.string.tea);
+        textViewAdditives.setText(getString(R.string.additional_to_drink, drink));
+        checkboxLemon.setVisibility(View.VISIBLE);
+        spinnerTea.setVisibility(View.VISIBLE);
+        spinnerCoffee.setVisibility(View.INVISIBLE);
+    }
+
+    private void onUsersChooseCoffee() {
+        drink = getString(R.string.coffee);
+        textViewAdditives.setText(getString(R.string.additional_to_drink, drink));
+        checkboxLemon.setVisibility(View.INVISIBLE);
+        spinnerTea.setVisibility(View.INVISIBLE);
+        spinnerCoffee.setVisibility(View.VISIBLE);
+    }
+
+    private void onUserMakeOrder() {
+        List<String> additives = new ArrayList<>();
+        if (checkboxSugar.isChecked()) {
+            additives.add(checkboxSugar.getText().toString());
+        }
+        if (radioButtonTea.isChecked() && checkboxLemon.isChecked()) {
+            additives.add(checkboxLemon.getText().toString());
+        }
+        if (checkboxMilk.isChecked()) {
+            additives.add(checkboxMilk.getText().toString());
+        }
+        String drinkType = "";
+        if (radioButtonTea.isChecked()) {
+            drinkType = spinnerTea.getSelectedItem().toString();
+        } else if (radioButtonCoffee.isChecked()) {
+            drinkType = spinnerCoffee.getSelectedItem().toString();
+        }
+        Intent intent = OrderDetailActivity.newIntent(MakeOrderActivity.this,
+                userName,
+                drink,
+                additives.toString(),
+                drinkType);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cafe/OrderDetailActivity.java
+++ app/src/main/java/com/example/cafe/OrderDetailActivity.java
@@ -2,13 +2,34 @@ package com.example.cafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class OrderDetailActivity extends AppCompatActivity {
+    public static final String EXTRA_USER_USERNAME = "userName";
+    public static final String EXTRA_USER_DRINK = "drink";
+    public static final String EXTRA_USER_ADDITIVES = "additives";
+    public static final String EXTRA_USER_DRINK_TYPE = "userName";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+    }
+
+    public static Intent newIntent(Context context,
+                                   String userName,
+                                   String drink,
+                                   String additives,
+                                   String drinkType) {
+        Intent intent = new Intent(context, OrderDetailActivity.class);
+        intent.putExtra(EXTRA_USER_USERNAME, userName);
+        intent.putExtra(EXTRA_USER_DRINK, drink);
+        intent.putExtra(EXTRA_USER_DRINK, additives);
+        intent.putExtra(EXTRA_USER_DRINK_TYPE, drinkType);
+        return intent;
     }
 }
