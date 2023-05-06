b085446c6fa8049a0d74dc531522c128eeba421f
alexMac <dedenevai3263@yandex.ru>
2023-05-06 14:33:26 +0300

    logic for first layout completed

--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -7,28 +7,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final EditText nameEditText = findViewById(R.id.nameEditText);
-    private final EditText passwordEditText = findViewById(R.id.passwordEditText);
-    private final String userName = nameEditText.getText().toString();
-    private final Button buttonSignIn = findViewById(R.id.enterButton);
+    private EditText nameEditText;
+    private EditText passwordEditText;
+    private Button buttonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
         buttonSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchScreen(userName);
+                String userName = nameEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+                if (userName.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(
+                            MainActivity.this,
+                            getString(R.string.error_field_is_empty),
+                            Toast.LENGTH_SHORT
+                    ).show();
+                } else {
+                    launchScreen(userName);
+                }
+
             }
         });
     }
-    private void launchScreen(String userName){
+
+    private void initViews() {
+        nameEditText = findViewById(R.id.nameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        buttonSignIn = findViewById(R.id.enterButton);
+
+    }
+
+    private void launchScreen(String userName) {
         Intent intent = new Intent(MainActivity.this, MakeOrderActivity.class);
-        intent.putExtra("userName",userName);
+        intent.putExtra("userName", userName);
         startActivity(intent);
     }
 }
